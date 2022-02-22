0696f6f7f126471cf81d3ed3d542a4eae74fde5d
MarshallPrimeLabs <99172161+MarshallPrimeLabs@users.noreply.github.com>
2022-02-22 17:46:56 +0000

    Update docker_entrypoint.sh

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -19,7 +19,13 @@ elif [ "$environment" = "production" ]; then
 
     echo "Running mainnnet..."
     ./diesel migration run && \
-    ./indexer-explorer --home-dir /indexer/near/mainnet run --store-genesis --stream-while-syncing --non-strict-mode --concurrency 100 sync-from-interruption --delta 500
+    ./indexer-explorer --home-dir /indexer/near/mainnet run --store-genesis --stream-while-syncing --non-strict-mode --concurrency 100 sync-from-block --height 9820214
+    
+elif [ "$environment" = "production-from-latest" ]; then
+
+    echo "Running mainnnet..."
+    ./diesel migration run && \
+    ./indexer-explorer --home-dir /indexer/nearlatest/mainnet run --store-genesis --stream-while-syncing --non-strict-mode --concurrency 1 sync-from-latest
 
 else
     exit 1
