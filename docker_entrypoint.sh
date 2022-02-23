05b324685781e1fcafcc6ac7522a3ae2c5a03e42
MarshallPrimeLabs <99172161+MarshallPrimeLabs@users.noreply.github.com>
2022-02-23 08:24:59 +0000

    Update docker_entrypoint.sh

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -19,13 +19,7 @@ elif [ "$environment" = "production" ]; then
 
     echo "Running mainnnet..."
     ./diesel migration run && \
-    ./indexer-explorer --home-dir /indexer/near/mainnet init ${BOOT_NODES:+--boot-nodes=${BOOT_NODES}} --chain-id mainnet --download-config --download-genesis && \
-    cd /indexer/near/mainnet && \
-    rm -rf config.json && \
-    wget https://s3-us-west-1.amazonaws.com/build.nearprotocol.com/nearcore-deploy/mainnet/config.json && \
-    sed -i 's/"archive": false/"archive": true/' /indexer/near/mainnet/config.json && \
-    cd /near/indexer-explorer && \
-    ./indexer-explorer --home-dir /indexer/near/mainnet run --store-genesis --stream-while-syncing --non-strict-mode --concurrency 100 sync-from-block --height 9820214
+    ./indexer-explorer --home-dir /indexer/near/mainnet run --store-genesis --stream-while-syncing --non-strict-mode --concurrency 100 sync-from-interruption --delta 500
 
 else
     exit 1
